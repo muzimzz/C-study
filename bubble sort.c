02f82735326c654689f343f32edd89f507dc9c28
yoonserin <rlagusdn0914@naver.com>
2021-04-29 21:28:24 +0900

    버블 정렬
    10 이하의 정수를 입력받고, 입력받은 정수 개수만큼의 정수를 입력받아 오름차순으로 정렬한다.
    이 때 버블정렬을 이용한다.

---
+++ bubble sort.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int i = 0, j = 0;
+
+void print(int *n, int input)
+{
+    int temp = 0;
+
+    for (i = 0; i < input; i++)
+        for (j = 0; j < input - 1; j++)
+        {
+            if (n[j] > n[j + 1])
+            {
+                temp = n[j];
+                n[j] = n[j + 1];
+                n[j + 1] = temp;
+            }
+        }
+
+    for (i = 0; i < input; i++)
+        printf("%d ", n[i]);
+}
+
+int main()
+{
+    int input = 0;
+    scanf("%d", &input);
+    int n[SIZE] = { 0 };
+
+    for (i = 0; i < input; i++)
+        scanf("%d", &n[i]);
+
+    print(n, input);
+
+    return 0;
+}
