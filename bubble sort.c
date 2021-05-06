061d1a6e30f113d7d6633e83182f0ec59ae36937
yoonserin <rlagusdn0914@naver.com>
2021-05-06 12:45:29 +0900

    버블정렬 수정
    원소의 개수가 i일 때, (0부터 i-1까지 반복을 하되
    이중 반복문의 경우 이미 정렬된 부분은 다시 정렬할 필요가 없다.
    따라서 for (j = 0; j<input -1; j++)를
    for (j = 0; j<input - (i + 1); j++로
    고쳐주었다.

--- bubble sort.c
+++ bubble sort.c
@@ -12,7 +12,7 @@ void print(int *n, int input)
     int temp = 0;
 
     for (i = 0; i < input - 1; i++)
-        for (j = 0; j < input - 1; j++)
+        for (j = 0; j < input - (i + 1); j++)
         {
             if (n[j] > n[j + 1])
             {
