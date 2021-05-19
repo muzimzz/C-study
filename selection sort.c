9b6bba546091a9f521bd4b1548cc83baea325472
yoonserin <rlagusdn0914@naver.com>
2021-05-19 20:30:10 +0900

    선택 정렬 (selection sort)
    선택정렬은 제자리 정렬 중 하나로, 해당 순서에 원소를 넣을 위치는 이미 정해져 있으며 어떤 원소를 넣을지 선택하는 알고리즘이다.
    첫 번째 순서에는 첫 번째 위치에 최소값을 넣는다.
    두 번째 순서에는 두 번째 위치에 남은 값 중 최소값을 넣는다.
    ...
    
    https://blog.naver.com/rlagusdn0914/222355788572

---
+++ selection sort.c
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define SWAP(x, y, temp) ( (temp)=(x), (x)=(y), (y)=(temp) )
+#define SIZE 5
+
+int i = 0, j = 0;
+
+
+void selection_sort(int* li, int n) 
+{
+    int least = 0, temp = 0;
+
+    for (i = 0; i < n - 1; i++)  // 마지막 숫자는 자동으로 정렬
+    {
+        least = i;
+
+        for (j = i + 1; j < n; j++)  // 최솟값 탐색
+            if (li[j] < li[least])
+                least = j; 
+    
+        if (i != least)  // 최솟값이 자기 자신이면 자료 이동 x
+            SWAP(li[i], li[least], temp);      
+    }
+
+    return;
+}
+
+int main() 
+{
+    int n = SIZE;
+    int li[SIZE] = { 0 };
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &li[i]);
+
+    selection_sort(li, n);
+
+    for (i = 0; i < n; i++)
+        printf("%d\n", li[i]);
+
+    return 0;
+}
