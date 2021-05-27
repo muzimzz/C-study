a8cf07de95ab7b67488620a706c33fc362d17c29
yoonserin <rlagusdn0914@naver.com>
2021-05-27 17:09:11 +0900

    정올 기초다지기 214
    <문제>
    
    10개의 단어를 입력받고 문자 하나를 더 입력 받는다.
    문자를 포함하는 단어를 모두 찾아 사전 순으로 출하시오.
    단어는 100자 이하다.
    * 표준입출력방식으로 작성하세요.
    
    <입력 예>
    
    hancom
    korea
    information
    monitor
    class
    member
    internet
    bible
    friends
    forever
    i
    
    <출력 예>
    
    bible
    friends
    information
    internet
    monitor

---
+++ JUNGOL/정올 214.c
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+//void insertion_sort(char word[], int count)
+//{
+//    int i = 0, j = 0;
+//    char temp[100] = "";
+//
+//    for (i = 1; i < count; i++)
+//    {
+//        strcpy(temp, word[i]);
+//
+//        for (j = i - 1; j >= 0 && word[j] < temp; j--)
+//            strcpy(word[j + 1], word[j]);
+//
+//        strcpy(word[j + 1], temp);
+//    }
+//
+//    return;
+//}
+
+int main()
+{
+    char word[10][100] = { 0 };
+    char choice[10][100] = { 0 };
+    char temp[100] = "";
+    int i = 0, j = 0, count = 0;
+    char n = '\0';
+
+    for (i = 0; i < 10; i++)
+        fgets(word[i], 100, stdin);
+
+    fscanf(stdin, "%c", &n);
+
+    for (i = 0; i < 10; i++)
+        if (strchr(word[i], n))
+        {
+            strcpy(choice[count], word[i]);
+            count++;
+        }
+
+    for (i = 1; i < count; i++)
+    {
+        strcpy(temp, choice[i]);
+
+        for (j = i - 1; j >= 0 && strcmp(choice[j], temp) > 0; j--)
+            strcpy(choice[j + 1], choice[j]);
+
+        strcpy(choice[j + 1], temp);
+    }
+
+    for (i = 0; i < count; i++)
+        printf("%s", choice[i]);
+
+    return 0;
+}
