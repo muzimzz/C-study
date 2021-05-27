46705b3aeec3446156890bba6dc40bac6b273cd7
yoonserin <rlagusdn0914@naver.com>
2021-05-27 17:07:36 +0900

    정올 기초다지기 213
    <문제>
    
    공백을 포함한 한 문장을 입력받아서 전체 길이와 가장 긴 단어를 출력하시오.
    단, 문장의 길이는 최대 100자 이내이고, 길이가 가장 긴 단어가 여러 개인 경우 공백으로 분리하여 한 줄에 입력 받은 순서대로 모두 출력하시오.
    이 문제에서의 단어란? 공백으로 분리하는 문자열을 뜻한다.
    "i am a boy." 문자열의 경우 "i", "am", "a", "boy."가 각각 단어이다.
    * 표준입출력방식으로 작성하세요.
    
    <입력 예>
    
    The largest organ in the body is the liver.
    
    <출력 예>
    
    43
    largest
    
    이상한 점: 주석처리를 해제하고 메인함수의 삽입정렬 코드 부분을
    insertion(word, count)로 바꾸면 제대로 실행이 되지 않음..
    왜 그러는 지 모르겠다 추후에 수정해야 할 듯

---
+++ JUNGOL/정올 213.c
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+//void insertion_sort(char word[], int count)
+//{
+//    int i = 0, j = 0;
+//    char temp[100] = "";
+//
+//    for (i = 1; i <= count; i++)
+//    {
+//        strcpy(temp, word[i]);
+//
+//        for (j = i - 1; j >= 0 && strlen(word[j]) < strlen(temp); j--)
+//            strcpy(word[j + 1], word[j]);
+//
+//        strcpy(word[j + 1], temp);
+//    }
+//
+//    return;
+//}
+
+int main()
+{
+    char str[100] = "";
+    char temp[100] = "";
+    char word[100][100] = { 0 };
+    int i = 0, j = 0, count = 0;
+
+    fgets(str, 100, stdin);
+
+    for (i = 0; i < strlen(str) - 1; i++)
+    {
+        if (str[i] == ' ')
+        {
+            j = 0;
+            count++;
+        }
+        else
+        {
+            word[count][j] = str[i];
+            j++;
+        }
+    }
+
+    for (i = 1; i <= count; i++)
+    {
+        strcpy(temp, word[i]);
+
+        for (j = i - 1; j >= 0 && strlen(word[j]) < strlen(temp); j--)
+            strcpy(word[j + 1], word[j]);
+
+        strcpy(word[j + 1], temp);
+    }
+
+    printf("%d\n", strlen(str));
+
+    for (i = 0; i <= count + 1; i++)
+        if (strlen(word[i]) == strlen(word[0]))
+            printf("%s ", word[i]);
+
+    return 0;
+}
