11af48554a39c1d5d4f957e15aeee57fa2be9195
yoonserin <rlagusdn0914@naver.com>
2021-07-31 19:36:01 +0900

    소수인지 아닌지 검사하는 코드 작성해보기
    입력한 수를 input이라 하고, 이가 소수인지 검사하기 위해 나눌 수를 i라고 한다.
    1과 자기 자신을 제외한 수로 나누어 0으로 떨어지지 않는 수를 소수라고 한다.
    i를 2부터 input-1까지 증가시켜 가면서 input을 i로 나누는 과정에서 나누어 떨어지면 소수가 아니다.
    반복문을 모두 종료했을 때 i가 input까지 증가되었으면 소수라고 출력한다.

---
+++ is_this_primeNumber.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void is_this_primeNumber()
+{
+	int input = 0, i = 0;
+
+		scanf("%d", &input);
+
+		for (i = 2; i < input; i++)
+		{
+			if (input % i == 0)
+			{
+				printf("%d는 소수가 아닙니다.\n", input);
+				break;
+			}
+		}
+
+		if (i == input)
+			printf("%d는 소수입니다.\n", input);
+
+	
+	return;
+}
+
+int main() 
+{
+	is_this_primeNumber();
+
+	return 0;
+}
